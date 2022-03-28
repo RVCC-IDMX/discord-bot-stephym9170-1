51817568d964a9b85434cb2c979f82b6257bfc69
Stephanie Mayers <g00286470@stu.raritanval.edu>
2022-03-27 21:59:04 -0400

    Add quotes

--- utils/cowsay.ts
+++ utils/cowsay.ts
@@ -1,16 +1,28 @@
 import cowsay from 'cowsay';
-import { IOptions } from 'cowsay';
+import { IOptions } from 'cowsay'; // optional
 import getRandomInt from './random';
+import quotes from './quotes.json';
 
-export default function () {
-    let opts: IOptions = {
-        text: 'Hello from typescript',
-        e: '^^',
-        r: true
-    }
-    let output: string = cowsay.say(opts);
-    output = `
+export default function (cow: string = 'random') {
+  //A random number is generated
+  const idx = getRandomInt(0, quotes.length);
+  const quoteOBJ = quotes[idx];
+  const text = `${quoteOBJ.quote} - ${quoteOBJ.author}`;
+
+  let opts: IOptions = {
+    text: text,
+    e: '^^',
+    r: true,
+    
+  };
+
+  if (cow !== 'random') {
+    opts.r = false;
+    opts.f = cow;
+  }
+  let output: string = cowsay.say(opts);
+  output = `
     \`\`\`${output}\`\`\`
     `;
-    return output;
+  return output;
 }
